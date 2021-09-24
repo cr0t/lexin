81b32dab793a1201788aebb959ca454f98c2193a
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2021-09-24 15:36:50 +0200

    Use ?query=... parameter from current URL
    We want to let users share their pages they're looking at the moment, so
    we started to rely on the `query` GET-parameter.
    
    In order to achieve that we introduced `handle_params/3` and
    `push_patch/2` LiveView features.
    
    We also added module documentation and refactored it a bit.

--- lib/lexin_web/live/dictionary_live.ex
+++ lib/lexin_web/live/dictionary_live.ex
@@ -1,23 +1,59 @@
 defmodule LexinWeb.DictionaryLive do
+  @moduledoc """
+  Main entrance point for our app. Provides a search form and shows results.
+
+  We want to support GET-params, so users are able to share links to the page they're looking at
+  any particular moment. So, we rely on the `?query` parameter a lot.
+
+  In order to handle it properly in LiveView we use `handle_params/3` which runs before `mount/3`.
+  """
+
   use LexinWeb, :live_view
 
   import LexinWeb.Gettext
 
   alias LexinWeb.CardComponent
 
-  def mount(_params, _session, socket) do
-    socket =
-      assign(socket, %{
-        query: "",
-        definitions: []
-      })
+  @doc false
+  def mount(_params, _session, socket), do: {:ok, socket}
 
-    {:ok, socket}
+  @doc """
+  When `?query=...` presented in the parameters hash, we use it as initial state and request data
+
+  By default (if user opens a page without query parameter) we set everything to clean state
+  """
+  def handle_params(params, uri, socket)
+
+  def handle_params(%{"query" => query}, _uri, socket) do
+    socket = assign(socket, :query, query)
+
+    {:noreply, find_definitions(socket)}
   end
 
+  def handle_params(_params, _uri, socket) do
+    socket = assign(socket, %{
+      query: "",
+      definitions: []
+    })
+
+    {:noreply, socket}
+  end
+
+  @doc """
+  Processes form submission by setting current URL to the new one (with updating `query` param),
+  the actual data request and handling happens in the corresponding `handle_params/3` function.
+  """
+  def handle_event(event, params, socket)
+
   def handle_event("search", %{"query" => query}, socket) do
-    socket =
-      case search(query) do
+    route = Routes.live_path(socket, LexinWeb.DictionaryLive, %{query: query})
+
+    {:noreply, push_patch(socket, to: route)}
+  end
+
+  defp find_definitions(socket = %{assigns: %{query: query}}) do
+    if String.length(query) > 0 do
+      case Lexin.Service.lookup(query) do
         {:ok, definitions} ->
           socket
           |> clear_flash()
@@ -28,13 +64,10 @@ defmodule LexinWeb.DictionaryLive do
           |> put_flash(:error, error_msg(err))
           |> assign(:definitions, [])
       end
-
-    socket = assign(socket, :query, query)
-
-    {:noreply, socket}
+    else
+      assign(socket, :definitions, [])
+    end
   end
-
-  defp search(query), do: Lexin.Service.lookup(query)
 
   defp error_msg(:not_found),
     do: dgettext("errors", "Not found")
