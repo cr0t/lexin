206122b568eccb2eb3caa7056b1199af3ed2b347
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2022-01-11 17:57:33 +0100

    Normalize search query (downcase)
    Users noticed that search is case-sensitive in some cases: while it
    works fine for English-like words, the case of the query affects queries
    in Russian. For example, "человек" gave the results, while "Человек" was
    empty.
    
    Maybe SQLite is case-sensitive for non-ASCII characters.
    
    We decided to downcase the search query before looking for definitions.

--- lib/lexin/dictionary.ex
+++ lib/lexin/dictionary.ex
@@ -5,14 +5,16 @@ defmodule Lexin.Dictionary do
 
   alias Lexin.Dictionary.Worker
 
-  @spec lookup(lang :: String.t(), word :: String.t()) ::
+  @spec lookup(lang :: String.t(), query :: String.t()) ::
           {:ok, [Lexin.Definition.t()]} | {:error, any()}
-  def lookup(lang, word) do
+  def lookup(lang, query) do
     try do
-      Worker.definitions(lang, word)
+      normalized_query = String.downcase(query)
+
+      Worker.definitions(lang, normalized_query)
     rescue
       error ->
-        information = %{lang: lang, word: word}
+        information = %{lang: lang, query: query}
         Sentry.capture_exception(error, stacktrace: __STACKTRACE__, extra: %{extra: information})
 
         {:error, :exception_processing_request}
