33e65c83ec684f5f7dd380d272b783ee24f3f51e
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2022-02-25 10:48:20 +0100

    Make application "feel" faster
    With the little improvement described in this blog post https://fly.io/phoenix-files/make-your-liveview-feel-faster/
    we can make app to feel faster for the end user.
    
    With these little tweaks on delayed scheduling of showing loading
    topbar, the search process feels almost instant.

--- assets/js/topbar.js
+++ assets/js/topbar.js
@@ -2,5 +2,19 @@ import topbar from '../vendor/topbar'
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: '#29d'}, shadowColor: 'rgba(0, 0, 0, .3)'})
-window.addEventListener('phx:page-loading-start', _info => topbar.show())
-window.addEventListener('phx:page-loading-stop', _info => topbar.hide())
+
+const SHOW_DELAY = 100 // ms
+
+let topbarScheduled
+
+window.addEventListener('phx:page-loading-start', _info => {
+  if (!topbarScheduled) {
+    topbarScheduled = setTimeout(() => topbar.show(), SHOW_DELAY)
+  }
+})
+
+window.addEventListener('phx:page-loading-stop', _info => {
+  clearTimeout(topbarScheduled)
+  topbarScheduled = undefined
+  topbar.hide()
+})
