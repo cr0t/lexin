42ff70549685322d635398a43576024fce5edcbb
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2022-09-05 22:51:57 +0200

    Bump Elixir/Erlang/Node.js/Alpine versions

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lexin.MixProject do
   def project do
     [
       app: :lexin,
-      version: "0.7.1",
+      version: "0.7.2",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
