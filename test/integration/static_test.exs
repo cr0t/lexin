7b99bcb7613b1169bb5d4ff5bbda26fd3a79b32e
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2022-09-08 23:17:22 +0200

    Cover static pages with integration tests

---
+++ test/integration/static_test.exs
@@ -0,0 +1,59 @@
+defmodule Lexin.StaticTest do
+  use ExUnit.Case, async: true
+  use Wallaby.Feature
+
+  import Wallaby.Query
+
+  feature "opens about page", %{session: session} do
+    session
+    |> visit("/")
+    |> click(link("About"))
+    |> assert_has(
+      css(".page_content",
+        text:
+          "Lexin – ett samarbete mellan Institutet för språk och folkminnen och Kungliga tekniska högskolan."
+      )
+    )
+    |> assert_has(
+      css(".page_content",
+        text:
+          "We have built this little application to improve user experience for end users. The functionality this app provides is very basic: pick a language and search for the word. That's simple!"
+      )
+    )
+  end
+
+  feature "opens contacts page", %{session: session} do
+    session
+    |> visit("/")
+    |> click(link("Contacts"))
+    |> assert_has(
+      css(".page_content",
+        text:
+          "Lexin Light is an open-source project, and you can find its code on GitHub. You are welcome to join the development process, or submit any feature requests or issues you find here."
+      )
+    )
+    |> assert_has(
+      css(".page_content",
+        text: "You can contact the author of this app by email or via LinkedIn."
+      )
+    )
+  end
+
+  feature "opens installation page", %{session: session} do
+    session
+    |> visit("/")
+    |> click(link("Install"))
+    |> assert_has(
+      css(".page_content",
+        text:
+          "You can use Lexin Light by opening it as a website in your favorite browser. (Psst... Save a bookmark for quick access!)"
+      )
+    )
+    |> assert_has(
+      css(".page_content",
+        text:
+          "You can also install and run Lexin Light as mobile application. See our step-by-step instructions on how to install this application to your iPhone/iPad. For Android-based devices installation process is similar, just do it using Chrome."
+      )
+    )
+  end
+end
