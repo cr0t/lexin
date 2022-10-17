8e680414be3056c951607c41e2c0948f6d56a6bc
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2022-10-17 18:05:09 +0200

    Update old 'Install' page screenshots (Light -> Mobi)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lexin.MixProject do
   def project do
     [
       app: :lexin,
-      version: "0.9.3",
+      version: "0.9.4",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
