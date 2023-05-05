b42ebe0807a6235808fa484a37cb6fd3ee8572a2
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-05-05 23:42:19 +0200

    Update config/*.ex files, align them with Phoenix 1.7

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,7 +6,28 @@ import Config
 # and secrets from environment variables or elsewhere. Do not define
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
+
+# ## Using releases
+#
+# If you use `mix release`, you need to explicitly enable the server
+# by passing the PHX_SERVER=true when you start it:
+#
+#     PHX_SERVER=true bin/exxy start
+#
+# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
+# script that automatically sets the env var above.
+if System.get_env("PHX_SERVER") do
+  config :lexin, LexinWeb.Endpoint, server: true
+end
+
 if config_env() == :prod do
+  # Directory with `swe_*.sqlite` dictionary files
+  dictionaries_root_path =
+    System.get_env("DICTIONARIES_ROOT") ||
+      raise "environment variable DICTIONARIES_ROOT is missing"
+
+  config :lexin, :dictionaries_root, dictionaries_root_path
+
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
@@ -19,32 +40,20 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  # Directory with `swe_*.sqlite` dictionary files
-  dictionaries_root_path =
-    System.get_env("DICTIONARIES_ROOT") ||
-      raise "environment variable DICTIONARIES_ROOT is missing"
+  # Where and what to listen to...
 
-  config :lexin, :dictionaries_root, dictionaries_root_path
+  host = System.get_env("PHX_HOST") || "lexin.mobi"
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :lexin, LexinWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: port
     ],
-    secret_key_base: secret_key_base,
-    server: true
-
-  # ## Using releases
-  #
-  # If you are doing OTP releases, you need to instruct Phoenix
-  # to start each relevant endpoint:
-  #
-  #     config :lexin, LexinWeb.Endpoint, server: true
-  #
-  # Then you can assemble a release by calling `mix release`.
-  # See `mix help release` for more information.
+    secret_key_base: secret_key_base
 end
