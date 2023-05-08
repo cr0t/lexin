1954b9eaa6da25f9cb5f5b04b2e853ecd6b2b14d
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-05-08 17:57:22 +0200

    Show search query in the page title
    We want to leverage the ability to distinguish Lexin results if user
    opened multiple tabs; to achieve this we used LiveView feature that
    allows us to update page's `<title>` tag content on the fly.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lexin.MixProject do
   def project do
     [
       app: :lexin,
-      version: "0.9.5",
+      version: "0.9.6",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
