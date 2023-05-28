650f7e1870670ae890af7ed9a7f7b9185e8880a5
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-05-28 22:29:05 +0200

    Tiny refactor to SerpComponents module (def->defp, module attributes, etc.)

--- lib/lexin_web/components/serp_components.ex
+++ lib/lexin_web/components/serp_components.ex
@@ -16,7 +16,9 @@ defmodule LexinWeb.SerpComponents do
   attr :target_lang, :string, required: true
   def definition(assigns)
 
-  @picture_languages %{
+  @audio_url_prefix Application.compile_env(:lexin, :external_audio_url_prefix, "")
+  @picture_url_prefix Application.compile_env(:lexin, :external_picture_url_prefix, "")
+  @picture_languages_map %{
     "albanian" => "swe,sqi",
     "amharic" => "swe,amh",
     "arabic" => "swe,ara",
@@ -40,35 +42,31 @@ defmodule LexinWeb.SerpComponents do
     "turkish" => "swe,tur"
   }
 
-  def graminfo(dfn),
+  defp graminfo(dfn),
     do: dfn.base.graminfo |> String.replace("&", dfn.value)
 
-  def illustrations(dfn),
+  defp illustrations(dfn),
     do: dfn.base.illustrations ++ dfn.target.illustrations
 
-  def inflections(dfn),
+  defp inflections(dfn),
     do: [dfn.value | Enum.map(dfn.base.inflections, & &1.value)]
 
-  def examples(dfn),
+  defp examples(dfn),
     do: Enum.zip(dfn.base.examples, dfn.target.examples)
 
-  def idioms(dfn),
+  defp idioms(dfn),
     do: Enum.zip(dfn.base.idioms, dfn.target.idioms)
 
-  def compounds(dfn),
+  defp compounds(dfn),
     do: Enum.zip(dfn.base.compounds, dfn.target.compounds)
 
-  def external_audio_url(audio_path) do
-    prefix = Application.get_env(:lexin, :external_audio_url_prefix)
+  defp external_audio_url(audio_path),
+    do: "#{@audio_url_prefix}#{audio_path}"
 
-    "#{prefix}#{audio_path}"
-  end
-
-  def external_picture_url(picture_params, target_lang) do
-    prefix = Application.get_env(:lexin, :external_picture_url_prefix)
-    langs_value = Map.get(@picture_languages, target_lang, "swe,swe")
+  defp external_picture_url(picture_params, target_lang) do
+    langs_value = Map.get(@picture_languages_map, target_lang, "swe,swe")
     languages = "languages=#{langs_value}"
 
-    "#{prefix}&#{languages}&#{picture_params}"
+    "#{@picture_url_prefix}&#{languages}&#{picture_params}"
   end
 end
