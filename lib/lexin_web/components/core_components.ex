55aab1d14cd16620822443a53016c2b930a72f06
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-05-28 13:04:00 +0200

    Tailwindize Flash component

--- lib/lexin_web/components/core_components.ex
+++ lib/lexin_web/components/core_components.ex
@@ -6,6 +6,7 @@ defmodule LexinWeb.CoreComponents do
   use Phoenix.Component
 
   alias Phoenix.LiveView.JS
+  import LexinWeb.Gettext
 
   @doc """
   Renders flash notices.
@@ -17,6 +18,7 @@ defmodule LexinWeb.CoreComponents do
   """
   attr :id, :string, default: "flash", doc: "the optional id of flash container"
   attr :flash, :map, default: %{}, doc: "the map of flash messages to display"
+  attr :title, :string, default: nil
   attr :kind, :atom, values: [:info, :error], doc: "used for styling and flash lookup"
   attr :rest, :global, doc: "the arbitrary HTML attributes to add to the flash container"
 
@@ -30,13 +32,28 @@ defmodule LexinWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "alert",
-        @kind == :info && "alert-info",
-        @kind == :error && "alert-danger"
+        "flash--container",
+        @kind == :info && "flash--info",
+        @kind == :error && "flash--error"
       ]}
       {@rest}
     >
-      <%= msg %>
+      <div class="flex items-start justify-between">
+        <div class="flex flex-col basis-full">
+          <div :if={@title} class="gap-1 my-1 text-sm font-semibold">
+            <.icon :if={@kind == :info} name="hero-information-circle-mini" class="h-4 w-4" />
+            <.icon :if={@kind == :error} name="hero-exclamation-circle-mini" class="h-4 w-4" />
+            <%= @title %>
+          </div>
+          <div class="my-1 text-base">
+            <%= msg %>
+          </div>
+        </div>
+
+        <button type="button" class="group flex self-center" aria-label={gettext("close")}>
+          <.icon name="hero-x-mark-solid" class="opacity-40 group-hover:opacity-70" />
+        </button>
+      </div>
     </div>
     """
   end
@@ -63,6 +80,33 @@ defmodule LexinWeb.CoreComponents do
     >
       We can't find the Internet, attempting to reconnect…
     </.flash>
+    """
+  end
+
+  @doc """
+  Renders a [Hero Icon](https://heroicons.com).
+
+  Hero icons come in three styles – outline, solid, and mini.
+  By default, the outline style is used, but solid an mini may
+  be applied by using the `-solid` and `-mini` suffix.
+
+  You can customize the size and colors of the icons by setting
+  width, height, and background color classes.
+
+  Icons are extracted from your `assets/vendor/heroicons` directory and bundled
+  within your compiled app.css by the plugin in your `assets/tailwind.config.js`.
+
+  ## Examples
+
+      <.icon name="hero-x-mark-solid" />
+      <.icon name="hero-arrow-path" class="ml-1 w-3 h-3 animate-spin" />
+  """
+  attr :name, :string, required: true
+  attr :class, :string, default: nil
+
+  def icon(%{name: "hero-" <> _} = assigns) do
+    ~H"""
+    <span class={[@name, @class]} />
     """
   end
 
