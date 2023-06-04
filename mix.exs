0e2333f228b547c638e4440d736d7f110e959272
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2023-06-04 23:55:46 +0200

    Bump Lexin app version to 0.11.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lexin.MixProject do
   def project do
     [
       app: :lexin,
-      version: "0.11.1",
+      version: "0.11.2",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
