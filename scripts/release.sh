6979e56e87fc5526bf35520933b4d146637da939
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-02-11 23:14:43 +0100

    Use Alpine as build and runner image again!
    According to some (missed earlier) news, Alpine's issues with DNS over
    TCP are nonsense anymore: https://www.theregister.com/2023/05/16/alpine_linux_318/
    
    So, let move back and use Alpine to preserve disk space and bandwidth
    everywhere (the final image is only ~35MB instead of ~120MB with Ubuntu).
    
    These changes mostly revert the ones from this commit:
    
    https://github.com/cr0t/lexin/commit/2fa52c56975341d107ccc0e3ec1c9a2c2371638e
    
    However, we also cleaned up a bit the Dockerfile.build along the way.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #
-# Build a Docker image and send it to remote machine to run it there.
+# Prepares data, shows notifications, and bootstraps the image build process.
+
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+NOCOLOR='\033[0m'
+echo -e "${RED}WARNING: ${ORANGE}Don't forget to pre-generate sitemaps and put them in the right place!${NOCOLOR}"
+
+# ---
 
 FULLPATH=$(realpath $0)
 SCRIPTS_DIR=$(dirname $FULLPATH)
 
+# retrieve app's name and its version to name the image we will make
 APP_NAME=$(grep 'app:' mix.exs | sed -e 's/\[//g' -e 's/ //g' -e 's/app://' -e 's/[:,]//g')
 APP_VERSION=$(grep 'version:' mix.exs | cut -d '"' -f2)
 
-# Pre-generate sitemap files on the host machine (ensure that all dictionaries available!)
-mix sitemap.gen
-
-# Time to build the image and push it to the registry
+# time to build the image and push it to the registry
 $SCRIPTS_DIR/build.sh $APP_NAME $APP_VERSION --force
