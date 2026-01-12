309e8a417af1b27e120fa7dd40bee272761681d2
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2026-01-12 20:29:34 +0100

    Start removing Tailwind CSS...
    ...as it was challenging to migrate it from v3 to v4 even in a such
    small application:
    
    - CSS specificity rules changed
    - some of the classes got renamed
    - automated migration didn't work out
    
    It seems that it's easier to use a simple CSS resetter and some
    handcrafted CSS to achieve long-term maintainability. Of course, we have
    just started by re-creating main layout, in the coming commits, we will
    continue cleaning of the components and codebase from Tailwind residues.

--- config/config.exs
+++ config/config.exs
@@ -28,17 +28,10 @@ config :esbuild,
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-  ]
-
-# Configure tailwind (the version is required)
-config :tailwind,
-  version: "4.1.18",
-  default: [
-    args: ~w(
-      --input=assets/css/app.css
-      --output=priv/static/assets/app.css
-    ),
-    cd: Path.expand("..", __DIR__)
+  ],
+  css: [
+    args: ~w(css/app.css --bundle --outdir=../priv/static/assets/),
+    cd: Path.expand("../assets", __DIR__)
   ]
 
 # Configures Elixir's Logger
