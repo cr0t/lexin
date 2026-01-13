38a8ff5db1e34410513f3b70710abb32f325d262
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2026-01-13 10:47:32 +0100

    Prettify (a bit) JS files

--- assets/js/topbar.js
+++ assets/js/topbar.js
@@ -2,7 +2,7 @@ import topbar from '../vendor/topbar'
 
 const SHOW_DELAY = 300 // ms
 
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", _info => topbar.show(SHOW_DELAY))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
